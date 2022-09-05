3437d317c2accca54fefab5a227b1f80f592c0a8
Nour Aldden Al Moqry <nour@aleksundshantu.com>
2022-09-05 12:40:37 +0200

    menu-card

---
+++ src/App.vue
@@ -0,0 +1,356 @@
+<template>
+  <div id="app">
+    <section class="js-horizontal-collapse horizontal-collapse mt-6">
+      <ul class="horizontal-collapse__inner">
+        <HorizontalCollapseItem className="horizontal-collapse__item--1">
+          <div class="horizontal-collapse__inactive-content">
+            <h2 class="horizontal-collapse__heading">Breakfast</h2>
+          </div>
+          <div class="horizontal-collapse__active-content">
+            <h2 class="horizontal-collapse__heading text-6xl">Breakfast</h2>
+            <div class="horizontal-collapse__menu">
+              <h2 class="text-3xl">Unser Menu</h2>
+              <spam class="order">Order des Tages:</spam>
+            </div>
+          <div class="flex justify-between text-xl items-center py-2 px-1">
+            <spam>- Best of Wurst- & Käseteller</spam>
+            <spam>12€</spam>
+          </div>
+          <spam class="text-x">Hausgemachte Konfitüre, eingelegtes Gemüse, Brot, Butter / KOBER </spam>
+          <div class="flex justify-between text-xl items-center py-2 px-1">
+            <spam>- Wurst- & Käseteller</spam>
+            <spam>9€</spam>
+          </div>
+          <spam class="text-x">Hausgemachte Konfitüre, Brot, Butter / KOBER </spam>
+          <div class="flex justify-between text-xl items-center py-2 px-1">
+            <spam>- Frühstücksburrito(vegan)</spam>
+            <spam>17€</spam>
+          </div>
+          <spam class="text-x">Veganes Tofurührei, Salsa Verde, frittierte Kartoffel</spam>
+          </div>
+        </HorizontalCollapseItem>
+
+        <HorizontalCollapseItem className="horizontal-collapse__item--2">
+          <div class="horizontal-collapse__inactive-content">
+            <h2 class="horizontal-collapse__heading">Lunch</h2>
+          </div>
+          <div class="horizontal-collapse__active-content">
+            <h2 class="horizontal-collapse__heading text-6xl">Lunch</h2>
+            <div class="horizontal-collapse__menu">
+              <h2 class="text-3xl">Unser Menu</h2>
+              <spam class="order">Order des Tages:</spam>
+            </div>
+            <div class="flex justify-between text-xl items-center py-2 px-1">
+            <spam>- Best of Wurst- & Käseteller</spam>
+            <spam>12€</spam>
+          </div>
+          <spam class="text-x">Hausgemachte Konfitüre, eingelegtes Gemüse, Brot, Butter / KOBER </spam>
+          <div class="flex justify-between text-xl items-center py-2 px-1">
+            <spam>- Wurst- & Käseteller</spam>
+            <spam>9€</spam>
+          </div>
+          <spam class="text-x">Hausgemachte Konfitüre, Brot, Butter / KOBER </spam>
+          <div class="flex justify-between text-xl items-center py-2 px-1">
+            <spam>- Frühstücksburrito(vegan)</spam>
+            <spam>17€</spam>
+          </div>
+          <spam class="text-x">Veganes Tofurührei, Salsa Verde, frittierte Kartoffel</spam>
+          </div>
+        </HorizontalCollapseItem>
+
+        <HorizontalCollapseItem className="horizontal-collapse__item--3">
+          <div class="horizontal-collapse__inactive-content">
+            <h2 class="horizontal-collapse__heading">Dinner</h2>
+          </div>
+          <div class="horizontal-collapse__active-content">
+            <h2 class="horizontal-collapse__heading text-6xl">Dinner</h2>
+            <div class="horizontal-collapse__menu">
+              <h2 class="text-3xl">Unser Menu</h2>
+              <spam class="order">Order des Tages:</spam>
+            </div>
+            <div class="flex justify-between text-xl items-center py-2 px-1">
+            <spam>- Best of Wurst- & Käseteller</spam>
+            <spam>12€</spam>
+          </div>
+          <spam class="text-x">Hausgemachte Konfitüre, eingelegtes Gemüse, Brot, Butter / KOBER </spam>
+          <div class="flex justify-between text-xl items-center py-2 px-1">
+            <spam>- Wurst- & Käseteller</spam>
+            <spam>9€</spam>
+          </div>
+          <spam class="text-x">Hausgemachte Konfitüre, Brot, Butter / KOBER </spam>
+          <div class="flex justify-between text-xl items-center py-2 px-1">
+            <spam>- Frühstücksburrito(vegan)</spam>
+            <spam>17€</spam>
+          </div>
+          <spam class="text-x">Veganes Tofurührei, Salsa Verde, frittierte Kartoffel</spam>
+          </div>
+        </HorizontalCollapseItem>
+
+        <HorizontalCollapseItem className="horizontal-collapse__item--4">
+          <div class="horizontal-collapse__inactive-content">
+            <h2 class="horizontal-collapse__heading">Weekends</h2>
+          </div>
+          <div class="horizontal-collapse__active-content">
+            <h2 class="horizontal-collapse__heading text-6xl">Weekends</h2>
+            <div class="horizontal-collapse__menu">
+              <h2 class="text-3xl">Unser Menu</h2>
+              <spam class="order">Order des Tages:</spam>
+            </div>
+            <div class="flex justify-between text-xl items-center py-2 px-1">
+            <spam>- Best of Wurst- & Käseteller</spam>
+            <spam>12€</spam>
+          </div>
+          <spam class="text-x">Hausgemachte Konfitüre, eingelegtes Gemüse, Brot, Butter / KOBER </spam>
+          <div class="flex justify-between text-xl items-center py-2 px-1">
+            <spam>- Wurst- & Käseteller</spam>
+            <spam>9€</spam>
+          </div>
+          <spam class="text-x">Hausgemachte Konfitüre, Brot, Butter / KOBER </spam>
+          <div class="flex justify-between text-xl items-center py-2 px-1">
+            <spam>- Frühstücksburrito(vegan)</spam>
+            <spam>17€</spam>
+          </div>
+          <spam class="text-x">Veganes Tofurührei, Salsa Verde, frittierte Kartoffel</spam>
+          </div>
+        </HorizontalCollapseItem>
+
+        <HorizontalCollapseItem className="horizontal-collapse__item--5">
+          <div class="horizontal-collapse__inactive-content">
+            <h2 class="horizontal-collapse__heading">Beverages</h2>
+          </div>
+          <div class="horizontal-collapse__active-content">
+            <h2 class="horizontal-collapse__heading text-6xl">Beverages</h2>
+            <div class="horizontal-collapse__menu">
+              <h2 class="text-3xl">Unser Menu</h2>
+              <spam class="order">Order des Tages:</spam>
+            </div>
+            <div class="flex justify-between text-xl items-center py-2 px-1">
+            <spam>- Kaffee schwarz</spam>
+            <spam>3€</spam>
+          </div>
+          <div class="flex justify-between text-xl items-center py-2 px-1">
+            <spam>- Saft</spam>
+            <spam>5€</spam>
+          </div>
+          <div class="flex justify-between text-xl items-center py-2 px-1">
+            <spam>- Wein</spam>
+            <spam>4€</spam>
+          </div>
+          </div>
+        </HorizontalCollapseItem>
+      </ul>
+    </section>
+    <div class="bg-gray-300 w-1/2 m-auto mb-12">
+      <div class="text-center border-b-2 border-black py-4">
+        <spam class="order text-3xl py-4">Menu des Tages:</spam>
+        <p class="bestellung text-xl pt-4" v-if="openTodos.length > 0">
+          Bestellte Order: {{ openTodos.length }}
+        </p>
+        <p class="bestellung text-xl pt-4" v-else>
+          Ihre Bestellung kommt in 20.min......
+        </p>
+        <div class="mt-4 border-2 border-black">
+          <input type="text" class="py-2 px-2 w-2/3 text-black" v-model="newTodo" />
+          <button class="bg-gray-500 py-2 w-1/3" @click="addTodo">
+            Potions Einfügen
+          </button>
+        </div>
+      </div>
+      <div v-for="(todo, index) in todos" :key="todo.todo">
+        <Todo
+          :todoprop="todo"
+          :todoindex="index"
+          @toggledone-index="toggleDone"
+          @removetodo-index="removeTodo"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import HorizontalCollapseItem from "../src/components/HorizontalCollapseItem.vue";
+import Todo from "../src/components/Todo.vue";
+
+export default {
+  name: "app",
+  data() {
+    return {
+      newTodo: "",
+      todos: [],
+    };
+  },
+  methods: {
+    toggleDone(index) {
+      this.todos[index].done = !this.todos[index].done;
+      this.localStorage;
+    },
+    removeTodo(index) {
+      this.todos.splice(index, 1);
+      this.localStorage;
+    },
+    addTodo() {
+      if (this.newTodo.trim() === "") {
+        return;
+      }
+      this.todos.push({ todo: this.newTodo, done: false });
+      this.localStorage;
+    },
+    saveTodos() {
+      localStorage.setItem("todos", JSON.stringify(this.todos));
+    },
+  },
+    mounted() {
+      let data = localStorage.getItem("todos");
+      if(data !== "" && data !== null) {
+        this.todos = JSON.parse(data);
+      } else {
+        this.todos = [];
+      }
+    },
+  computed: {
+    openTodos() {
+      const openTodos = this.todos.filter((todo) => {
+        return !todo.done;
+      });
+      return openTodos;
+    },
+  },
+  components: {
+    HorizontalCollapseItem,
+    Todo,
+  },
+};
+</script>
+
+<style lang="scss">
+$transitionDurationMs: 0.5s;
+
+.horizontal-collapse {
+  width: 100%;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  margin-bottom: 5rem;
+  color: #1a2b56;
+
+  *,
+  *:before,
+  *:after {
+    box-sizing: border-box;
+  }
+
+  &__inner {
+    padding: 0;
+    margin: 0;
+    display: flex;
+    flex-direction: row;
+  }
+
+  &__item {
+    min-height: 42rem;
+    list-style: none;
+    padding: 0;
+    overflow: hidden;
+    min-width: 10rem;
+    max-width: 40rem;
+    position: relative;
+    transition: width $transitionDurationMs;
+
+    &--1 {
+      background-color: #09d8a2;
+    }
+
+    &--2 {
+      background-color: #67eac2;
+    }
+
+    &--3 {
+      background-color: #75aae5;
+    }
+
+    &--4 {
+      background-color: #1479d5;
+    }
+
+    &--5 {
+      background-color: #1853bb;
+    }
+
+    &.is-active {
+      // color: #fff;
+
+      .horizontal-collapse__inactive-content {
+        opacity: 0;
+      }
+
+      .horizontal-collapse__active-content {
+        opacity: 1;
+      }
+    }
+  }
+
+  &__item-inner {
+    padding: 3rem;
+  }
+
+  &__inactive-content {
+    opacity: 1;
+    transition: opacity $transitionDurationMs;
+    position: absolute;
+    bottom: 2rem;
+    left: 1rem;
+
+    .horizontal-collapse__heading {
+      user-select: none;
+      cursor: default;
+      font-size: 4rem;
+      line-height: 0;
+      margin-left: 4rem;
+      writing-mode: vertical-rl;
+      text-orientation: mixed;
+      transform: rotate(180deg);
+    }
+  }
+
+  &__active-content {
+    opacity: 0;
+    transition: opacity $transitionDurationMs;
+    cursor: default;
+  }
+
+  &__heading {
+    margin-top: 0;
+    margin-bottom: 2rem;
+  }
+
+  &__body {
+    margin-bottom: 2rem;
+  }
+
+  &__link {
+    display: inline-block;
+    background-color: #fff;
+    color: #333;
+    text-decoration: none;
+    line-height: 1;
+    padding: 1rem 2rem;
+    border-radius: 2rem;
+    font-size: 1.7rem;
+    font-weight: bold;
+  }
+
+  .horizontal-collapse__menu {
+    text-align: center;
+    border-bottom: solid 4px #000000;
+  }
+  .order {
+    font-size: 1.3rem;
+    font-family: Verdana, Geneva, Tahoma, sans-serif;
+  }
+  .bestellung {
+    text-align: center;
+  }
+}
+</style>
